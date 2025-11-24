eff914bb031c5f3517d1f27966ea9bf6c81baa22
Krzysztof Worwa <worwak1998@gmail.com>
2025-11-24 13:36:42 +0100

    add animation to Burger button

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.querySelector(".burger-menu");
+    const burger = document.getElementById("burger");
     const mobileMenu = document.getElementById("mobile-menu");
 
     if (!burger || !mobileMenu) return;
 
     burger.addEventListener("click", () => {
         const isOpen = burger.getAttribute("aria-expanded") === "true";
-
         burger.setAttribute("aria-expanded", String(!isOpen));
+
+        burger.classList.toggle("open");
 
         mobileMenu.classList.toggle("hidden");
     });
