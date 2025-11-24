29151814cb8d6280d2578aa22fb4fd3bdd2d4010
Krzysztof Worwa <worwak1998@gmail.com>
2025-11-24 10:50:10 +0100

    add tailwind, dependencies, header, navigation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
