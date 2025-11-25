3d192d1971840bf46ffb9df0aca47d1a0c8cf42f
Krzysztof Worwa <worwak1998@gmail.com>
2025-11-25 16:24:42 +0100

    deleting kaystatic integration for githubpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   site: 'https://krzysiekworwa.github.io/neura/',
   base: '/neura/',
-  output: 'static', // konieczne dla GitHub Pages
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
   integrations: [
     react(),
     markdoc(),
-    keystatic()
   ]
 });
